453c4a02b5258e1231e21f12902a66b7b6776289
Thane Chaves <tmoonder@gmail.com>
2021-10-23 14:08:21 -0400

    Created the 16 x 16 grid using flexbox, changed up my initial method.

--- javascript.js
+++ javascript.js
@@ -2,44 +2,17 @@ const grid = document.createElement("div");
 grid.classList.add("gridclass");
 document.body.appendChild(grid);
 
-const row = document.createElement("div");
-row.classList.add("rowclass");
-grid.appendChild(row);
-
-const row2 = document.createElement("div");
-row2.classList.add("rowclass");
-grid.appendChild(row2);
-
-let nameOfRow;
-
-function createRow () {
-
-const div1 = document.createElement("div");
-div1.classList.add("boxclass");
-div1.textContent = "box one";
-nameOfRow.appendChild(div1);
-
-const div2 = document.createElement("div");
-div2.classList.add("boxclass");
-div2.textContent = "box two";
-nameOfRow.appendChild(div2);
-
-const div3 = document.createElement("div");
-div3.classList.add("boxclass");
-div3.textContent = "box three";
-nameOfRow.appendChild(div3);
-
-const div4 = document.createElement("div");
-div4.classList.add("boxclass");
-div4.textContent = "box four";
-nameOfRow.appendChild(div4);
-
+function createBox () {
+    let box = document.createElement("div");
+    box.classList.add("boxclass");
+    box.textContent = "*";
+    grid.appendChild(box);
 }
 
-nameOfRow = row;
+function createGrid () {
+for (i = 1; i <= 256; i++) {
+    createBox();
+    }
+}
 
-createRow(nameOfRow);
-
-nameOfRow = row2;
-
-createRow(nameOfRow);
+createGrid();
